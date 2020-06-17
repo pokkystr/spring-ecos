5e610d1fa38375f49be1abe26029dcb965c8d827
pokkystr <pokky_str@hotmail.com>
2020-06-17 21:51:19 +0700

    relocaltion folder

---
+++ GCP-PubSub/singlePubSub/src/main/java/com/example/pubusub/pubsubDemo/PubsubDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.pubusub.pubsubDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubsubDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PubsubDemoApplication.class, args);
+	}
+
+}
---
+++ Kafka/kafkaConsumer/src/main/java/com/example/kafka/receiving/Listener.java
@@ -0,0 +1,19 @@
+package com.example.kafka.receiving;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class Listener {
+    private static final Logger logger = LogManager.getFormatterLogger(Listener.class);
+
+    @KafkaListener(topics = "pigke.message", groupId = "demo1", concurrency = "5")
+    public void consume(String message) throws IOException {
+        logger.info("Consume Message: %s", message);
+    }
+
+}
